d5bbd044f5f7bd9b25c3e4c17efb1cbd01ae8276
Pham Thi Thuy Linh <thuylinhcua2024@gmail.com>
2024-10-14 00:22:26 +0700

    Add a new file: Learn to use backticks to write string

---
+++ JSCore/template-strings.js
@@ -0,0 +1,8 @@
+const name = "Thuy Linh";
+let age = 20;
+
+//Using double or single quotation marks
+console.log("Hi, I am "+ name+" and I am "+ age +" years old.");
+
+//Using backticks
+console.log(`Hi, I am ${name} and I am ${age} years old.`);
